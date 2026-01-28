b8740dbe709d4b47fc18d9d5476452e8f24764e5
YusufDurak <yunturneddrk@gmail.com>
2026-01-28 19:07:41 +0300

    Checkpoint

--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -21,7 +21,6 @@ public class VFXManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
